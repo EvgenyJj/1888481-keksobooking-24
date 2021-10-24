68ea0f0ec66f5ce93e66e5a69fa22d7e8b5f3459
Евгений <redov64@gmail.com>
2021-10-24 16:59:58 +0800

    Модуляция

---
+++ js/util.js
@@ -0,0 +1,18 @@
+export function getRandomPositiveInteger (min, max) {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+getRandomPositiveInteger(0, 10);
+
+export function getRandomPositiveFloat (min, max, digits = 1) {
+  const lower = Math.min(Math.abs(min), Math.abs(max));
+  const upper = Math.max(Math.abs(min), Math.abs(max));
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(digits);
+}
+getRandomPositiveFloat(1.1, 1.2, 2);
+
+export const getRandomArrayElement = (elements) => elements[_.random(0, elements.length - 1)];
+getRandomArrayElement();
