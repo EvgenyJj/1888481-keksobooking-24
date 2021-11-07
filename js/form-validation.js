3a6dc0ce4fff103dc862c6844491b093f39fb176
Евгений <redov64@gmail.com>
2021-11-07 14:48:41 +0800

    Правда или действие (часть 1)

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,14 +1,13 @@
 const MIN_TITLE_LENGTH = 30;
 const MAX_TITLE_LENGTH = 100;
 //const MAX_PRICE_IN_NIGHT = 1000000;
-
+const MAX_ROOMS = 100;
 
 const adForm = document.querySelector('.ad-form');
 const formTitle = adForm.querySelector('#title');
 //const formPrice = adForm.querySelector('#price');
 const formRooms = adForm.querySelector('#room_number');
 const formCapacity = adForm.querySelector('#capacity');
-const capacityElements = formCapacity.querySelectorAll('option');
 
 formTitle.addEventListener('input', () => {
   const valueLength = formTitle.value.length;
@@ -24,46 +23,20 @@ formTitle.addEventListener('input', () => {
   formTitle.reportValidity();
 });
 
-formRooms.addEventListener('change', () => {
-  if (formRooms.value === '1') {
-    formCapacity.value = '1';
-    capacityElements.forEach((capacityElement) => {
-      if (capacityElement.value !== '1') {
-        capacityElement.disabled = true;
-      } else {
-        capacityElement.disabled = false;
-      }
-    });
-  } else if (formRooms.value === '2') {
-    formCapacity.value = '1';
-    capacityElements.forEach((capacityElement) => {
-      if (capacityElement.value === '0' || capacityElement.value === '3') {
-        capacityElement.disabled = true;
-      } else {
-        capacityElement.disabled = false;
-      }
-    });
-  } else if (formRooms.value === '3') {
-    formCapacity.value = '1';
-    capacityElements.forEach((capacityElement) => {
-      if (capacityElement.value === '0') {
-        capacityElement.disabled = true;
-      } else {
-        capacityElement.disabled = false;
-      }
-    });
-  } else if (formRooms.value === '100') {
-    capacityElements.forEach((capacityElement) => {
-      capacityElement.value = '0';
-      if (capacityElement.value !== '0') {
-        capacityElement.disabled = true;
-      } else {
-        capacityElement.disabled = false;
-      }
-    });
+const checkCapacity = () => {
+  const rooms = Number(formRooms.value);
+  const guests = Number(formCapacity.value);
+  if (rooms < guests) {
+    formCapacity.setCustomValidity('Неподходящий вариант, выберите другой.');
+  } else if (rooms === MAX_ROOMS && guests !== 0) {
+    formCapacity.setCustomValidity('Неподходящий вариант, выберите другой.');
+  } else if (guests === 0 && rooms !== MAX_ROOMS) {
+    formCapacity.setCustomValidity('Неподходящий вариант, выберите другой.');
   } else {
-    capacityElements.forEach((capacityElement) => {
-      capacityElement.disabled = false;
-    });
+    formCapacity.setCustomValidity('');
   }
-});
+  formCapacity.reportValidity();
+};
+
+formCapacity.addEventListener('change', checkCapacity);
+formRooms.addEventListener('chancg', checkCapacity);
